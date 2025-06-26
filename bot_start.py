73dd89dee7c0d1ce38f757d96e95b759c35cfb9c
Taminkom <108420867+Taminkom@users.noreply.github.com>
2025-06-26 22:02:23 +0200

    Update bot_start.py

--- bot_start.py
+++ bot_start.py
@@ -1,18 +1,1 @@
-import telebot
-import os
 
-BOT_TOKEN = os.environ['BOT_TOKEN']
-bot = telebot.TeleBot(BOT_TOKEN)
-
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    if message.text == '/start':
-        bot.reply_to(message, "Cześć tutaj Twój tradingowy BOT, mówi mi T4BB. Sprawdzam sygnały na giełdzie MEXC, dla par BTCUSDT.P, ETHUSDT.P, SOLUSDT.P, DOGEUSDT.P ❤️")
-
-# Ignoruj wszystkie inne wiadomości
-@bot.message_handler(func=lambda message: True)
-def ignore_all(message):
-    pass
-
-if __name__ == "__main__":
-    bot.polling(none_stop=True)
